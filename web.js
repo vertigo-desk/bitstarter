9f886621418cafe537d7422b487fee8e88b0a9dc
vertigo <vertigo.num@gmail.com>
2013-07-11 11:46:45 +0000

    fs

--- web.js
+++ web.js
@@ -1,9 +1,8 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFileSync(index.html).toString()
     response.send(fs.readFileSync(index.html).toString());
 });
 
