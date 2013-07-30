f889bebc1f0426dc23b0514e5d1336dbad11994f
vertigo-desk <vertigo.num@gmail.com>
2013-07-30 14:35:00 +0000

    geoloc

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/js'));
+
 app.get('/', function(request, response) {
     response.send(fs.readFileSync("index.html").toString());
 });
